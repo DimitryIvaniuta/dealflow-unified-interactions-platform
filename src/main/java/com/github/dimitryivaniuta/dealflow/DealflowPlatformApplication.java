6137598d0cdf4032df5dea3477f48db86213e4ee
Dimitry Ivaniuta <dzmitry.ivaniuta.services@gmail.com>
2026-01-18 22:00:24 +0100

    Deal flow platform application

---
+++ src/main/java/com/github/dimitryivaniuta/dealflow/DealflowPlatformApplication.java
@@ -0,0 +1,12 @@
+package com.github.dimitryivaniuta.dealflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealflowPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DealflowPlatformApplication.class, args);
+    }
+}
